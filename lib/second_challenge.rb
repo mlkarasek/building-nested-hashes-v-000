2a7ef265afda95956c4b49a082b8e00dba2674d0
Monica  Karasek <mlkarasek@gmail.com>
2017-10-21 21:09:28 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,17 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-    :patriarch => {},
-    :matriarch => {},
-    :hero => {},
-    :hero_friends => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       },
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => {}
+      :heroine_friends =>[]
+      }
     }
 
 
